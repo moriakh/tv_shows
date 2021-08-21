187da0125e7e660ddc4cfc745d1022228b820233
Moriak Nunez <moriaknc@outlook.com>
2021-08-20 22:05:52 -0400

    second part

--- tv_shows/models.py
+++ tv_shows/models.py
@@ -1,16 +1,33 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class ShowsManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Show title should be at least 2 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Show description should be at least 10 characters"
+        try:
+            if datetime.strptime(postData['date_show'], "%Y-%m-%d").date() > datetime.today().date():
+                errors['date_show'] = 'you have to put an older date'
+        except:
+            errors['date_show'] = 'Set a valid date'
+        return errors
+
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     release_date = models.DateField()
     desc = models.TextField(default="some tv show")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    objects = ShowsManager()
 
 class Networks(models.Model):
     name = models.CharField(max_length=255)
     shows = models.ForeignKey(Shows, related_name="networks", on_delete = models.CASCADE, null = True, blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from tv_shows.models import Shows, Networks
+from django.contrib import messages
 
 def index(request):
     context = {
@@ -33,12 +34,20 @@ def add_show(request):
     network_id = int(request.POST['network_id'])
     network = Networks.objects.get(id=network_id)
 
-    s_title = request.POST['title']
-    s_release_date = request.POST['release_date']
-    s_desc = request.POST['desc']
-    show = Shows.objects.create(title = s_title, release_date = s_release_date, desc = s_desc)
-    show.networks.add(network)
-    return redirect(request.META.get('HTTP_REFERER'))
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('../new')
+    else:
+        s_title = request.POST['title']
+        s_release_date = request.POST['release_date']
+        s_desc = request.POST['desc']
+        show = Shows.objects.create(title = s_title, release_date = s_release_date, desc = s_desc)
+        show.networks.add(network)
+        messages.success(request, "Show successfully created")
+
+        return redirect('/shows')
 
 def destroy(request, tv_show_id):
     show = Shows.objects.get(id=tv_show_id)
@@ -49,15 +58,22 @@ def destroy(request, tv_show_id):
 def update(request, tv_show_id):
     network_id = request.POST['network_id']
     network = Networks.objects.get(id=network_id)
+    errors = Shows.objects.basic_validator(request.POST)
 
-    show = Shows.objects.get(id=tv_show_id)
-    show_title = request.POST['title']
-    show_date = request.POST['date']
-    show_desc = request.POST['desc']
-    show.title = show_title
-    show.release_date = show_date
-    show.desc = show_desc
-    show.networks.add(network)
-    show.save()
-    return redirect(request.META.get('HTTP_REFERER'))
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'../{tv_show_id}/edit')
+    else:
+        show = Shows.objects.get(id=tv_show_id)
+        show_title = request.POST['title']
+        show_date = request.POST['date']
+        show_desc = request.POST['desc']
+        show.title = show_title
+        show.release_date = show_date
+        show.desc = show_desc
+        show.networks.add(network)
+        show.save()
+        messages.success(request, "Show successfully updated")
+        return redirect('/shows')
 
